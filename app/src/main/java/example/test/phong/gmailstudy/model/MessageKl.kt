edd4259cea90c8459c281e985aa7893fb7742dd1
Phong Huỳnh <p7157568@gmail.com>
2017-07-29 19:16:07 +0700

    feat - add the core code with rcv and activity

--- app/src/main/java/example/test/phong/gmailstudy/model/MessageKl.kt
+++ app/src/main/java/example/test/phong/gmailstudy/model/MessageKl.kt
@@ -5,13 +5,13 @@ package example.test.phong.gmailstudy.model
  * <a href="https://medium.com/@DarrenAtherton/intro-to-data-classes-in-kotlin-7f956d54365c"
  * info - When we specify the data keyword in our class definition, Kotlin automatically generates field accessors, hashCode(), equals(), toString(), as well as the useful copy() and componentN() functions (more on these later).
  */
-data class MessageKl(private val id: Int,
-                     private var from: String,
-                     private var subject: String,
-                     private var message: String,
-                     private var timestamp: String,
-                     private var picture: String,
-                     private var isImportant: Boolean,
-                     private var isRead: Boolean,
-                     private var color: Int
+data class MessageKl( val id: Int,
+                      var from: String,
+                      var subject: String,
+                      var message: String,
+                      var timestamp: String,
+                      var picture: String,
+                      var isImportant: Boolean,
+                      var isRead: Boolean,
+                      var color: Int
                      )
